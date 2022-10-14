00a79f0f7b200cd06a9f52bc741e100df7af2d66
Kaushal559 <92305096+Kaushal559@users.noreply.github.com>
2022-10-15 00:17:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var canvas = new fabric.Canvas('myCanvas');
+
  var x= document.getElementById("myAudio");
 
 function new_image()
@@ -6,13 +6,6 @@ function new_image()
 	fabric.Image.fromURL('Birthdayimage.jpg',function(Img) {
         block_image_object= Img;
 
-        block_image_object.scaleToWidth(700);
-        block_image_object.scaleToHeight(510);
-        block_image_object.set({
-            top:0,
-            left:0
-        });
-        canvas.add(block_image_object);
     });
 
     
